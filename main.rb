18a0467b8a3166dded42edac567feeb33bfb60d6
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-09-27 21:41:33 +0430

    Remove extra space in main file

--- main.rb
+++ main.rb
@@ -8,7 +8,6 @@ puts "Any? (greater than 5): #{list.any? { |item| item > 5 }}" # Output: false
 filtered_list = list.filter(&:even?)
 puts "Filtered List (even numbers): #{filtered_list}"
 
-
 list = MyList.new(1, 2, 3, 4)
 puts(list.all? { |e| e < 5 })
 puts(list.all? { |e| e > 5 })
